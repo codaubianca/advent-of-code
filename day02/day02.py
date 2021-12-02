088fdd358aaf8e283469ec400d0c3451d4c8c212
codaubianca <codau_bianca@yahoo.com>
2021-12-02 20:13:27 +0100

    Added solution for part two of day 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,13 +1,14 @@
 from typing import List, Tuple
 
 def calculate_submarine_position(moves: List[Tuple[str, int]]) -> Tuple[int, int]:
-    horizontal_position, depth = 0, 0
+    horizontal_position, depth, aim = 0, 0, 0
     for i in range(len(moves)):
         move, units = moves[i]
         if move == 'forward':
             horizontal_position += units
+            depth += aim * units
         elif move == 'down':
-            depth += units
+            aim += units
         elif move == 'up':
-            depth -= units
+            aim -= units
     return (horizontal_position, depth)
