34522bd6712fc1bb42cbe66e70132ff1ed8872d3
codaubianca <codau_bianca@yahoo.com>
2022-06-20 10:39:39 +0200

    Added solution to day 09 part two

--- day09/day09.py
+++ day09/day09.py
@@ -1,4 +1,6 @@
-from typing import List
+from audioop import mul
+from lib2to3.pgen2.literals import simple_escapes
+from typing import List, Tuple
 
 TEST = "2199943210\n3987894921\n9856789892\n8767896789\n9899965678"
 
@@ -29,21 +31,63 @@ def _find_low_points(lines: List[List[int]]):
         #print("new line")
         for j in range(1, column_limit):
             minima = min([padded_lines[i-1][j], padded_lines[i+1][j], padded_lines[i][j-1], padded_lines[i][j+1]])
-            print(f"elem: {padded_lines[i][j]}, minim: {minima}")
+            #print(f"elem: {padded_lines[i][j]}, minim: {minima}")
             if padded_lines[i][j] < minima:
                 low_points += [(i-1, j-1)]
                 print(f"i: {i}, j: {j}, elem: {padded_lines[i][j]}")
 
     return low_points
 
-def  calculate_risk(lines: List[List[int]]) -> int:
+def calculate_risk(lines: List[List[int]]) -> int:
     low_points = _find_low_points(lines)
     return sum([lines[i][j] + 1 for (i, j) in low_points])
 
- 
+def _get_size_of_basin(padded_lines: List[List[int]], low_point: Tuple[int, int], basin: List[Tuple[int,int]]):
+
+    i, j = low_point
+
+    if (i, j) in basin:
+        return 0
+
+    if padded_lines[i][j] == 9:
+        return 0
+
+    basin += [low_point]
+
+    return 1 + _get_size_of_basin(padded_lines, (i - 1, j), basin) + _get_size_of_basin(padded_lines, (i + 1, j), basin) \
+        + _get_size_of_basin(padded_lines, (i, j - 1), basin) + _get_size_of_basin(padded_lines, (i, j + 1), basin)
+    
+def multiply_all_basin_sizes(lines: List[List[int]]):
+    padded_lines = _add_padding(lines)
+    low_points = _find_low_points(lines)
+    sizes = []
+    for i,j in low_points:
+        size_of_basin = _get_size_of_basin(padded_lines, (i+1, j+1), [])
+        print(size_of_basin)
+        sizes += [size_of_basin]
+    
+    sizes = sorted(sizes, reverse=True)
+       
+    return sizes[0] * sizes[1] * sizes[2]
+
+def test_part_one():
+    lines = parse_lines(TEST.split('\n'))
+    assert calculate_risk(lines) == 15
+
+
+def test_part_two():
+    lines = parse_lines(TEST.split('\n'))
+    assert multiply_all_basin_sizes(lines) == 1134
+
 
 lines = read_file("inputs/day09.txt")
-#lines = parse_lines(lines)
-lines = parse_lines(TEST.split('\n'))
+lines = parse_lines(lines)
+
+# PART ONE
 total_risk = calculate_risk(lines)
-print(f"The sum of the risk levels of all points is {total_risk}")
+print(f"The sum of the risk levels of all points is {total_risk}.")
+
+# PART TWO
+product = multiply_all_basin_sizes(lines)
+print(f"Product of sizes of three largest basins: {product}.")
+
